02b3f5b58fccb12be1fb60efd3bffc5b0a8fa0ed
Maksim <maksimpooh@gmail.com>
2019-04-16 11:23:14 +0300

    commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <app-table></app-table>
+</template>
+
+<script>
+import AppTable from './components/Table'
+
+export default {
+  components: {
+    AppTable
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,54 @@
+<template>
+<v-container>
+  <v-layout>
+    <v-flex>
+      <v-data-table
+        :items="crypto"
+        class="elevation-1"
+        hide-actions
+        hide-headers
+      >
+        <template v-slot:items="props">
+          <td class="text-xs-left"><img style="width: 30px" :src="source + props.item.CoinInfo.ImageUrl" alt=""></td>
+          <td class="text-xs-left">{{ props.item.CoinInfo.FullName }}</td>
+          <td class="text-xs-left">{{ props.item.CoinInfo.Name }}</td>
+          <td class="text-xs-left">{{ props.item.RAW.USD.PRICE }}</td>
+          <td class="text-xs-left">{{ props.item.RAW.USD.SUPPLY }}</td>
+        </template>
+      </v-data-table>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        crypto: [],
+        source: 'https://www.cryptocompare.com'
+      }
+    },
+    computed: {
+      sortPrices() {
+        return crypto.sort((a, b) => a.RAW.USD.PRICE - b.RAW.USD.PRICE)
+      }
+    },
+    mounted() {
+      axios
+        .get('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD')
+        .then(response => (this.crypto = response.data.Data))
+      setInterval(() => {
+      axios
+        .get('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD')
+        .then(response => (this.crypto = response.data.Data))
+      }, 300000)
+    }
+  }
+</script>
+
+<style>
+
+</style>
